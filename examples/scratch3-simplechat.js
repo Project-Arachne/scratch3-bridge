4c1e48ac2acf47e18bde737788e78b5deb1d612a
John Locke <51202569+J0hnL0cke@users.noreply.github.com>
2020-05-03 10:15:13 -0400

    Allow connecting two projects
    Initial commit, still in testing

--- examples/scratch3-simplechat.js
+++ examples/scratch3-simplechat.js
@@ -1,24 +1,26 @@
-
 const readline = require('readline');
 const ScratchBridge = require('../scratch3-bridge');
 
-let msgrbridge = new ScratchBridge();
-//Alternative (no user prompts made):
-//let msgrbridge = new ScratchBridge( "ascratchuser", "theuserpassword",PROJECT_ID);
+const projectOne = 391068012;
+const projectTwo = 391078105;
+let msgrbridge1 = new ScratchBridge(process.env.username, process.env.password, projectOne);
+let msgrbridge2 = new ScratchBridge(process.env.username, process.env.password, projectTwo);
 
-
-
-msgrbridge.connect();
+msgrbridge2.connect();
+msgrbridge1.connect();
 let sender;
 
-msgrbridge.on('data', (msg,asender) =>{
+//Project 1 server
+
+msgrbridge1.on('data', (msg,asender) =>{ //when the server gets a message
     console.log(`>(${asender}): ${msg}`);
     sender = asender;
-    msgrbridge.send( sender, `String reversed on the server: ${msg.toString().split('').reverse().join('')}`  );
+    msgrbridge1.send( sender, `Data recieved: ${msg.toString()}`);
+    msgrbridge2.send( sender, `Project1 says: ${msg.toString()}`); //respond to the project when the server gets data
 });
 
-msgrbridge.on('connect', () => {
-    console.log( "Connected!! Sending your messages to Scratch:")
+msgrbridge1.on('connect', () => {
+    console.log( "Connected!! Sending your messages to Project1:")
     
     const stdinput = readline.createInterface({
         input: process.stdin,
@@ -26,13 +28,40 @@ msgrbridge.on('connect', () => {
     });
 
     stdinput.on('line', (msg) => {
-        
-        msgrbridge.send( sender, `Message written on the server: ${msg}` );
+        sender= 0;
+        msgrbridge1.send( sender, `Server says: ${msg}` ); //what to send to the client
+        msgrbridge2.send( sender, `Server says: ${msg}` ); //what to send to the client
         if(msg === "quit"){
+        
             stdinput.close();
         }
     })
 
 })
 
+//Project 2 server
 
+msgrbridge2.on('data', (msg,asender) =>{
+    console.log(`>(${asender}): ${msg}`);
+    sender = asender;
+    msgrbridge2.send( sender, `Data recieved: ${msg.toString()}`);
+    msgrbridge1.send( sender, `Project2 says: ${msg.toString()}`); //respond to the project when the server gets data
+});
+
+msgrbridge2.on('connect', () => {
+    console.log( "Connected!! Sending messages to Project2")
+    
+    //const stdinput = readline.createInterface({
+    //    input: process.stdin,
+    //    output: process.stdout
+    //});
+
+    //stdinput.on('line', (msg) => {
+        
+    //    msgrbridge2.send( sender, `Server says: ${msg}` ); //what to send to the client
+    //    if(msg === "quit"){
+    //        stdinput.close();
+    //    }
+    //})
+
+})
